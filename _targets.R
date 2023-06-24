f06c80f75e842631674e49de0f40c69abcc240d0
Rob Hyndman <robjhyndman@gmail.com>
2023-06-24 15:07:08 +1000

    Fixed all spacing issues and added caching back

--- _targets.R
+++ _targets.R
@@ -2,17 +2,26 @@
 library(targets)
 library(tarchetypes)
 
+# Files called by all qmd files
+extra_files <- c(
+  "setup.R",
+  "_quarto.yml",
+  "header.tex",
+  "beamerthememonash.sty",
+  "beamercolorthememonashwhite.sty"
+)
+
 list(
   tar_quarto(readme, "README.qmd"),
-  tar_quarto(ch0, "0-intro.qmd", extra_files = c("setup.R","_quarto.yml")),
-  tar_quarto(ch1, "1-tsibbles.qmd", extra_files = c("setup.R","_quarto.yml")),
-  tar_quarto(ch2, "2-graphics.qmd", extra_files = c("setup.R","_quarto.yml")),
-  tar_quarto(ch3, "3-transformations.qmd", extra_files = c("setup.R","_quarto.yml")),
-  tar_quarto(ch4, "4-decompositions.qmd", extra_files = c("setup.R","_quarto.yml")),
-  tar_quarto(ch5, "5-feasts.qmd", extra_files = c("setup.R","_quarto.yml")),
-  tar_quarto(ch6, "6-fable.qmd", extra_files = c("setup.R","_quarto.yml")),
-  tar_quarto(ch7, "7-ets.qmd", extra_files = c("setup.R","_quarto.yml")),
-  tar_quarto(ch8, "8-arima.qmd", extra_files = c("setup.R","_quarto.yml")),
-  tar_quarto(ch9, "9-dynamic-regression.qmd", extra_files = c("setup.R","_quarto.yml")),
-  tar_quarto(ch10, "10-reconciliation.qmd", extra_files = c("setup.R","_quarto.yml"))
+  tar_quarto(ch0, "0-intro.qmd", extra_files = extra_files),
+  tar_quarto(ch1, "1-tsibbles.qmd", extra_files = extra_files),
+  tar_quarto(ch2, "2-graphics.qmd", extra_files = extra_files),
+  tar_quarto(ch3, "3-transformations.qmd", extra_files = extra_files),
+  tar_quarto(ch4, "4-decompositions.qmd", extra_files = extra_files),
+  tar_quarto(ch5, "5-feasts.qmd", extra_files = extra_files),
+  tar_quarto(ch6, "6-fable.qmd", extra_files = extra_files),
+  tar_quarto(ch7, "7-ets.qmd", extra_files = extra_files),
+  tar_quarto(ch8, "8-arima.qmd", extra_files = extra_files),
+  tar_quarto(ch9, "9-dynamic-regression.qmd", extra_files = extra_files),
+  tar_quarto(ch10, "10-reconciliation.qmd", extra_files = extra_files)
 )
